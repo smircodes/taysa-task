3dacead5170df2e989e7c22b344bca2d133bdb0e
SamiraMiss <samirarostami71@gmail.com>
2025-09-29 00:09:55 +0330

    add router for dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,15 @@
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Dashboard() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      router.push("/login");
+    }
+  }, [router]);
+
   return <h1>Welcome to Dashboard 🎉</h1>;
 }
