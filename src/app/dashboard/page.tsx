57ad506249cb446cb50d5c196efa3d0d8a219cd8
SamiraMiss <samirarostami71@gmail.com>
2025-09-29 13:28:29 +0330

    add complete dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,34 @@
 "use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
 
+type User = {
+  id: number;
+  email: string;
+};
 export default function Dashboard() {
-  const router = useRouter();
+  const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (!token) {
-      router.push("/login");
+    const userInfo = Cookies.get("user_info");
+    if (userInfo) {
+      const userData = JSON.parse(userInfo);
+      setUser(userData);
     }
-  }, [router]);
+  }, []);
 
-  return <h1>Welcome to Dashboard 🎉</h1>;
+  return (
+    <div>
+      <h1>Welcome to Dashboard 🎉</h1>
+      {!user ? (
+        <p>user data is loading...</p>
+      ) : (
+        <div>
+          <h2>Welcome {user.email}!</h2>
+          <p>This is your dashboard.</p>
+          <p>Your user ID is: {user.id}</p>
+        </div>
+      )}
+    </div>
+  );
 }
